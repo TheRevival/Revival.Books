0bbdec4018b4ffd5a9845be793d91d29e6c8d7c5
binary robot <vitalii.revival@gmail.com>
2021-01-14 11:26:10 +0200

    All needed project templates

---
+++ Revival.Books.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Revival.Books.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ Revival.Books.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Revival.Books.Services
+{
+    public class Class1
+    {
+    }
+}
